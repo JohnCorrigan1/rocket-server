2d92dec1ed8dd1877223a6796fd0442b572e85c2
Chad Bro <chad@chadbro.lan>
2024-06-11 16:19:06 -0700

    file transfers

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 #[macro_use]
 extern crate rocket;
+use rocket::data::{Limits, ToByteUnit};
+use rocket::form::Form;
+use rocket::fs::TempFile;
 use rocket::{
     serde::{json::Json, Serialize},
     tokio::time::sleep,
 };
-use std::str::FromStr;
-use std::time::Duration;
-//use rocket::data::{Data, ToByteUnit};
-//use rocket::http::{ContentType, Status};
-//use std::fs::File;
-//use std::io::Write;
-//use sys_info;
-//use rocket_contrib::json::Json;
-//use rocket_contrib::json::Json;
-
-use rocket::http::Method;
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
-
+use std::os::unix::fs::PermissionsExt;
+use std::{fs, path::Path, time::Duration};
 use sysinfo::{CpuRefreshKind, Disks, MemoryRefreshKind, System};
 
 #[derive(Serialize)]
@@ -37,8 +29,15 @@ async fn cpu() -> Json<CpuInfo> {
     })
 }
 
+#[derive(Serialize)]
+struct MemoryInfo {
+    total_memory: f64,
+    used_memory: f64,
+    memory_usage: f64,
+}
+
 #[get("/memory")]
-async fn memory() -> String {
+async fn memory() -> Json<MemoryInfo> {
     let system = System::new_with_specifics(
         sysinfo::RefreshKind::new().with_memory(MemoryRefreshKind::everything()),
     );
@@ -47,47 +46,92 @@ async fn memory() -> String {
     let used_memory = system.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0; // in GB
     let memory_usage = used_memory / total_memory * 100.0;
 
-    format!(
-        "{{totalMemory: {:.2} usedMemory: {:.2}, memoryUsage: {:.2}}}",
-        &total_memory, &used_memory, &memory_usage
-    )
+    Json(MemoryInfo {
+        total_memory,
+        used_memory,
+        memory_usage,
+    })
+}
+
+#[derive(Serialize)]
+struct Storage {
+    name: String,
+    disk_type: String,
+    total: f64,
+    available: f64,
+    usage: f64,
+    mount_point: String,
+    file_system: String,
 }
 
 #[get("/storage")]
-async fn storage() -> String {
+async fn storage() -> Json<Vec<Storage>> {
     let disks = Disks::new_with_refreshed_list();
     let disk_usage = disks
         .iter()
+        .filter(|disk| {
+            disk.file_system().to_str().unwrap() == "ext4"
+                && !disk.mount_point().to_str().unwrap().starts_with("/var")
+        })
         .map(|disk| {
             let available = disk.available_space() as f64 / 1024.0 / 1024.0 / 1024.0; // in GB
             let total = disk.total_space() as f64 / 1024.0 / 1024.0 / 1024.0; // in GB
-            let name = disk.name().to_str();
-            let usage = (total - available) / total * 100.0;
-            let disk_type = disk.kind().to_string();
-            //let mount_point = disk.mount_point().to_str().to_string();
 
-            format!(
-                "{{name: {}, type: {}, total: {:.2}, available: {:.2}, usage: {:.2}}}",
-                name.unwrap(),
-                disk_type,
+            Storage {
+                name: disk.name().to_str().unwrap().to_string(),
+                disk_type: disk.kind().to_string(),
                 total,
                 available,
-                usage
-            )
+                usage: (total - available) / total * 100.0,
+                mount_point: disk.mount_point().to_str().unwrap().to_string(),
+                file_system: disk.file_system().to_str().unwrap().to_string(),
+            }
         })
-        .collect::<Vec<String>>()
-        .join(",\n");
+        .collect();
+    Json(disk_usage)
+}
 
-    format!("[{}]", disk_usage)
+#[derive(FromForm)]
+struct ImageUpload<'r> {
+    image: TempFile<'r>,
+}
+
+#[post("/upload", data = "<form>", format = "multipart/form-data", rank = 1)]
+async fn upload(mut form: Form<ImageUpload<'_>>) -> Result<String, std::io::Error> {
+    let filename = match form.image.name() {
+        Some(name) => name.to_string(),
+        None => "image.jpeg".to_string(),
+    };
+
+    let content_type = match form.image.content_type() {
+        Some(content_type) => content_type.media_type().extension().unwrap().as_str(),
+        None => ".mkv",
+    };
+
+    let path = Path::new("/mnt/sdb1").join(format!("{}.{}", &filename, content_type));
+    form.image.persist_to(&path).await?;
+
+    let mut perms = fs::metadata(&path)?.permissions();
+    perms.set_mode(0o744); //rwxr--r-- read write execute for owner, read for others
+    fs::set_permissions(&path, perms)?;
+    Ok(format!("{} uploaded successfully", filename))
 }
 
 #[launch]
 fn rocket() -> _ {
     let cors = rocket_cors::CorsOptions::default().to_cors().unwrap();
+    let config = rocket::config::Config {
+        address: std::net::Ipv4Addr::new(0, 0, 0, 0).into(),
+        temp_dir: "/mnt/sdb1/temp".into(),
+        limits: Limits::new()
+            .limit("file", 100.gigabytes())
+            .limit("image", 100.megabytes())
+            .limit("data-form", 100.gigabytes()),
+        ..Default::default()
+    };
 
     rocket::build()
+        .configure(config)
         .attach(cors)
-        .mount("/api", routes![cpu, memory, storage])
+        .mount("/api", routes![cpu, memory, storage, upload])
 }
-
-//#[post("/upload", data = "<data>")]
